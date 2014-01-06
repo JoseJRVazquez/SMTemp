140d03085f65b1921b9de1ac7ffd8f2463bb03b7
jr vazquez <kwikteck@gmail.com>
2014-01-06 11:03:54 -0600

    First Commit of SM Temp
    Front page for collecting data

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sm1::Application.config.secret_key_base = '8c0964a92af78b47253ed5d3edb4eb0f076dc35c84572ad489248b07bcc8efa803e8d17fa23b857046fadabce2f2de782b0aa33d72c19b65aa53f92ee5b3ffa4'
